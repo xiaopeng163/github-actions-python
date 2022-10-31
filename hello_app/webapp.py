11eb08e7bca44435907aab744761e603a4b8e8e7
Peng Xiao <xiaoquwl@gmail.com>
2022-10-31 22:15:50 +0100

    init

---
+++ hello_app/webapp.py
@@ -0,0 +1,7 @@
+# Entry point for the application.
+from . import app    # noqa For application discovery by the 'flask' command.
+from . import views  # noqa For import side-effects of setting up routes.
+
+# Time-saver: output a URL to the VS Code terminal so you
+# can easily Ctrl+click to open a browser
+# print('http://127.0.0.1:5000/hello/VSCode')
